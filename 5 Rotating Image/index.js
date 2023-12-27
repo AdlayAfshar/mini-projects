4868cbd0ef30a135a71a63493ce66e3cfa994afe
Yalda Afshar <adlayafshar@gmail.com>
2023-12-27 15:57:30 +0000

    rotating img

---
+++ 5 Rotating Image/index.js
@@ -0,0 +1,29 @@
+const imgContainer = document.querySelector('.img-container');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let x = 0;
+let timer;
+
+prev.addEventListener('click', () => { 
+    x += 45;
+    clearTimeout(timer);
+    updateGallery();
+})
+
+next.addEventListener('click', () => { 
+    x -= 45;
+    clearTimeout(timer);
+    updateGallery();
+})
+
+function updateGallery() {
+    imgContainer.style.transform = `perspective(1000px) rotateY(${x}deg)`
+    timer =  setTimeout(()=> {
+        x -= 45;
+        updateGallery();
+    }, 3000)
+ }
+
+
+ updateGallery();
