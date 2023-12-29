476a8da981462432a0c8c5f32e4bdd9d3c6610f2
Yalda Afshar <adlayafshar@gmail.com>
2023-12-29 07:14:50 +0000

    not copmlete!

---
+++ 39 Anime Pics Generator/index.js
@@ -0,0 +1,9 @@
+// anime-container
+const btn = document.getElementById('btn');
+const animeContainer = document.querySelector('.anime-container');
+const animeImg = document.querySelector('.anime-img');
+const animeName = document.querySelector('.anime-name');
+
+btn.addEventListener('click', () => {
+    
+})
