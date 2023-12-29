fa39506e840ae722cfb2f01ff8fdac357a55b1d6
Yalda Afshar <adlayafshar@gmail.com>
2023-12-29 05:29:31 +0000

    add detailes to counter project

--- 37 Counter/index.js
+++ 37 Counter/index.js
@@ -1,22 +1,26 @@
 let num = 0;
 
-const value = document.querySelector('.value');
-const btns = document.querySelectorAll('.btn');
+const value = document.querySelector(".value");
+const btns = document.querySelectorAll(".btn");
 
 btns.forEach((btn) => {
-    btn.addEventListener('click', (event) => {
-   
-       const styles = event.currentTarget.classList;
-       console.log(styles);
-       if (styles.contains('btn-danger')) {
-        num--;
-       } else if (styles.contains('btn-success')){
-        num++;
-       } else {
-        num = 0;
-       }
-       value.textContent = num;
-       console.log(value);
+  btn.addEventListener("click", (event) => {
+    const styles = event.currentTarget.classList;
+    if (styles.contains("btn-danger")) {
+      num--;
+    } else if (styles.contains("btn-success")) {
+      num++;
+    } else {
+      num = 0;
+    }
+    value.textContent = num;
 
-    })
-})
+    if (num > 0) {
+      value.style.color = "green";
+    } else if (num < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "black";
+    }
+  });
+});
