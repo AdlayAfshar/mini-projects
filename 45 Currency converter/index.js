69a2110bf17a598ac3f03cff79c6cccec959d0a4
Yalda Afshar <adlayafshar@gmail.com>
2023-12-30 00:31:33 +0000

    complete currency converter project

--- 45 Currency converter/index.js
+++ 45 Currency converter/index.js
@@ -4,12 +4,30 @@ const worthFirst = document.getElementById("worth-first");
 const worthSecond = document.getElementById("worth-second");
 const exchange = document.getElementById("exchange");
 
-function updateRate() {
+updateRate();
 
+function updateRate() {
+  fetch(
+    `https://v6.exchangerate-api.com/v6/7953bfcc291e10306da483cf/latest/${currencyFirst.value}`
+  )
+    // if we use {}, we must return
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      const rate = data.conversion_rates[currencySecond.value];
+      console.log(rate);
+      exchange.innerText = `1 ${currencyFirst.value} = ${
+        rate + "" + currencySecond.value
+      }`;
+
+      worthSecond.value = (worthFirst.value * rate).toFixed(3);
+    });
 }
 
-currencyFirst.addEventListener('change', updateRate);
-currencySecond.addEventListener('change', updateRate);
+currencyFirst.addEventListener("change", updateRate);
+currencySecond.addEventListener("change", updateRate);
 
-worthFirst.addEventListener('change', updateRate);
-worthSecond.addEventListener('change', updateRate);
+worthFirst.addEventListener("change", updateRate);
+worthSecond.addEventListener("change", updateRate);
